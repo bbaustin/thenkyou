13116cad0add54b147d49735b0086f1f0003d4a6
Ben Austin <BenjaminBoydAustin@gmail.com>
2016-08-14 14:58:32 -0500

    Readings create libraries with reading_id and vocab_id.

--- app/controllers/go_controller.rb
+++ app/controllers/go_controller.rb
@@ -4,21 +4,53 @@ class GoController < ApplicationController
 ##########################
   post '/adminreading' do 
     @reading = Reading.create content: params['content'], difficulty: params['difficulty'], language: params['language']
-    @library = Library.create reading_id: @reading['id'] 
-    p 'vvvvvvvvvv this is the new library info vvvvvvvvvvvv'
-    p @reading['id']
-    # p @library 
-    # p Library 
+    # check language;
+    # check wordLang1,2,or 3 based on above
+    # by looping through @reading.content and looking for the word
+    # create a library with vocab_id of the found vocab.
+    @vocabs = Vocab.all 
+    if @reading.language == "English"
+      @vocabs.each do |v|
+        # p "---------------------------------------"
+        # puts @reading.content
+        # p v.wordLang1
+        # p v.wordLang1
+        # p @reading.content.index()
+        # p "---------------------------------------"
+        if v.wordLang1
+          if @reading.content.index(v.wordLang1) != nil 
+            @library = Library.create reading_id: @reading['id'], vocab_id: v['id']
+          end
+        end
+      end
+    elsif @reading.language == "日本語"
+      @vocabs.each do |v|
+        if @reading.content.include? v.wordLang3
+          @library = Library.create reading_id: @reading['id'], vocab_id: v['id']
+        end
+      end
+    end 
+
+
+    # puts stuff here if you want
     # Binding.pry
     erb :adminreading
   end
 
   get '/adminreading' do
+    p 'yooooooo!~~~~~~~~~~~'
+    @vocabs = Vocab.all 
+    @vocabs.each do |v|
+      puts v.wordLang1
+      puts v.wordLang2
+      puts v.wordLang3
+    end
     erb :adminreading
   end
 ###
   post '/adminvocab' do
     @vocab = Vocab.create wordLang1: params['wordLang1'], wordLang2: params['wordLang2'], wordLang3: params['wordLang3'], defLang1: params['defLang1'], defLang2: params['defLang2'], defLang3: params['defLang3'], usageLang1: params['usageLang1'], usageLang2: params['usageLang2'], usageLang3: params['usageLang3'], posLang1: params['posLang1'], posLang2: params['posLang2'], posLang3: params['posLang3']
+    @library = Library.create vocab_id: @vocab['id'] 
     erb :adminvocab
   end
 
@@ -39,8 +71,7 @@ class GoController < ApplicationController
     Reading.all.each do |reading|
       @reading_list.push(["#{reading['id']}. #{reading['content']}"])
     end
-    puts @reading_list
-    puts "good evening"
+    puts "Why is this showing up during adminreading POST?"
     @reading_list.to_json
   end  
 
