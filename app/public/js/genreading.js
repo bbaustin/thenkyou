4f49f3554672ad6960776fff086bb545c551f900
Ben Austin <BenjaminBoydAustin@gmail.com>
2016-08-15 14:14:50 -0500

    Vocabs visually appended to readings

--- app/public/js/genreading.js
+++ app/public/js/genreading.js
@@ -1,16 +1,49 @@
 // ../go/..
 // Generate attributes of reading selections.
 
+
+var combining = function(splitArray) {
+  for (var i = 0; i < splitArray.length; i+=2) {
+    var stringWithClass = splitArray[i].join(splitArray[i+1]);  
+    }
+    return stringWithClass;
+};
+
+var splitting = function(vocab, content) {
+  var strArr = [];
+  console.log(vocab.length);
+  console.log(vocab);
+  console.log(typeof vocab);
+  console.log(content);
+  console.log(typeof content);
+  for (var i = 0; i < vocab.length; i++){
+    if (content.indexOf(vocab[i].wordLang1) !== -1) { // NEW!!!!! FOR ENGLISH 
+      strArr.push(content.split(vocab[i].wordLang1)); //adds everything but vocabs
+      strArr.push(['<span class="vocab">' + vocab[i].wordLang1 + '</span>']); //adds vocabs [with style]
+      //console.log(strArr);
+      content = combining(strArr);
+    }
+
+  }
+  return strArr;
+};
+
+
+
+
 var myId = parseInt($("#myId").text())
 
 $.ajax({
-  url: '/go/' + myId, // dynamic url. how? 
+  url: '/go/' + myId, // dynamic url
   type: 'post',
   dataType: 'json',
   success: function(response) {
     console.log(typeof response);
     console.log(response);
     console.log("new response");
+    var showParagraph = combining(splitting(response, $("#reading-main").text()));
+    console.log(showParagraph);
+    $("#reading-main").html(showParagraph);
   },
   error: function(error) {
     console.log("error: " + error);
@@ -18,3 +51,9 @@ $.ajax({
 });
 
 
+
+
+
+// var showParagraph = combining(splitting(response, $("reading-main").text()));
+// console.log(showParagraph);
+// $('#readingPassage').append(showParagraph);
