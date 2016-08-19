bfe89e5b47489a0392d6e61d22d4e481d059d088
Ben Austin <BenjaminBoydAustin@gmail.com>
2016-08-19 03:59:03 -0500

    Final small style changes.

--- app/public/js/gengo.js
+++ app/public/js/gengo.js
@@ -9,13 +9,7 @@ $.ajax({
     console.log("Pushing from gengo!");
     for (var i = 0; i < response.length; i++) {
       $('.button_container').append(response[i]); 
-    } 
-//red: rgb(253,141,141)
-//rgb(255,177,72)
-//rgb(255,214,14)
-//rgb(124,233,56)
-//rgb(82,191,255)
-//rgb(226,180,249)
+  }
 
     var colorList = ["rgba(253,141,141,.6)", "rgba(255,177,72,.6)", "rgba(255,214,14,.6)", "rgba(124,233,56,.6)", "rgba(82,191,255,.6)", "rgba(226,180,249,.6)", "#979797"];
     var reading_buttons = document.getElementsByClassName('reading_button');
